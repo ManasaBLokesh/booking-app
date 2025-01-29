9a85d4c49eae61fc2626fc831d3760c8ed42593e
ManasaBLokesh <manasabl2000@gmail.com>
2025-01-29 19:15:34 +0530

    Your commit message here

--- data.js
+++ data.js
@@ -2,15 +2,15 @@ exports.module = [
   {
     "Sl_no": 1,
     "Room_no": 101,
-    "active": "N",
-    "Number_of_guests": 0,
-    "Booked_on": null,
-    "check_in_date": null,
-    "check_out_date": null,
-    "number_of_days": 0,
-    "Name": null,
-    "email_id": null,
-    "phone_no": null
+    "active": "Y",
+    "Number_of_guests": 1,
+    "Booked_on": "2025-01-29T13:44:12.760Z",
+    "check_in_date": "2025-01-31",
+    "check_out_date": "2025-02-01",
+    "number_of_days": 1,
+    "Name": "Adhithya",
+    "email_id": "adhithya@gmail.com",
+    "phone_no": " 9178276726378"
   },
   {
     "Sl_no": 1,
